9dc0de8aab1d12ce6a7cfce97772b39861df51fe
PROYECTO JA <kendall.torres.17@gmail.com>
2026-02-18 17:16:27 -0600

    versión 1.0.105

--- main.js
+++ main.js
@@ -253,7 +253,7 @@ app.on("window-all-closed", () => {
 });
 
 app.on("activate", () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
+  if (app.isReady() && BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
